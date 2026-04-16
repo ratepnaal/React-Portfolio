e8fc59ef874fb28d55680c33aff88277c385c620
ratepnaal <163038280+ratepnaal@users.noreply.github.com>
2026-04-17 01:20:04 +0300

    Fix import path for project6 image

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,7 +9,7 @@ import project2 from '../assets/images/project2.png'
 import project3 from '../assets/images/project3.png'
 import project4 from '../assets/images/project4.png'
 import project5 from '../assets/images/project5.png'
-import project6 from '../assese/images/project6.png'
+import project6 from '../assets/images/project6.png'
 import project_person from '../assets/images/project_person.png'
 
 const Project = () => {
